d228c365a652b42018822385ccc9d7047c34854c
codemvs <codemvs@gmail.com>
2018-08-15 13:04:10 -0500

    Ejemplo practico uso de Yargs

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -9,24 +9,49 @@ const argv = require('yargs')
                      default:10
                  }
              })
+             .command('crear','Genera un archivo con la tabla de multiplicar',{
+                 base: {
+                     demand: true,
+                     alias: 'b'
+                 },
+                 limite: {
+                     alias: 'l',
+                     default: 10
+                 }
+             })
              .help()
              .argv;
-const {crearArchivo} = require('./multiplicar/multiplicar');
+const { crearArchivo, listarTabla} = require('./multiplicar/multiplicar');
 
 //leer parametros.
-let argv2 = process.argv;
+// let argv2 = process.argv;
 // let parametro = argv[2];
 // let base = parametro.split('=')[1];
 
-console.log("Base: ",argv.base);
-console.log("Limite: ",argv.base);
+// console.log("Base: ",argv.base);
+// console.log("Limite: ",argv.base);
 
-return;
+let comando = argv._[0];
 
-crearArchivo(base)
-    .then(archivo=>{
-        console.log(`El archivo ${archivo} se ha creado.`);        
-    })
-    .catch(err=>{
-        console.log("Error:",err);        
-    });
+switch (comando) {
+    case 'listar':
+        console.log(argv.base, comando);
+    
+        listarTabla(argv.base, argv.limite);      
+        break;
+    case 'crear':
+        console.log(argv);
+        
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => {
+                console.log(`El archivo ${archivo} se ha creado.`);
+            })
+            .catch(err => {
+                console.log("Error:", err);
+            });
+        break;
+    default:
+        console.log('comando no reconocido');        
+        break;
+}
+
--- 03-bases-node/multiplicar/multiplicar.js
+++ 03-bases-node/multiplicar/multiplicar.js
@@ -2,26 +2,36 @@
  * Requireds
  */
 const fs = require('fs');
-
-let crearArchivo = (base)=>{
+/**
+ * crear archivo txt
+ * @param {*} base 
+ */
+let crearArchivo = (base, limite=10)=>{
     return new Promise((resolve,reject)=>{
         if(!Number(base)){
             reject(`El valor ingresado ${base}.`);
             return;
         }
         let data = '';
-        for (let i = 1; i <= 10; i++) {
+        for (let i = 1; i <= limite; i++) {
             data += `${base} * ${i} = ${base * i}\n`;
         }
 
-        fs.writeFile(`./tablas/tabla-${base}.txt`, data, err => {
+        fs.writeFile(`./tablas/tabla-${base}-al-${limite}.txt`, data, err => {
             if (err) reject(err);
             else
-                resolve(`tabla-${base}.txt`);
+                resolve(`tabla-${base}-al-${limite}.txt`);
         });
     });
 }
 
+let listarTabla = (base, limite=10)=>{
+    for (let i = 1; i <= limite; i++) {
+        console.log(`${base} x ${i} = ${base*i}`);                
+    }
+}
+
 module.exports = {
-    crearArchivo
+    crearArchivo,
+    listarTabla
 }
