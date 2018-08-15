330e0f714da91e2fea8ca391728b0fcac249a7cb
codemvs <codemvs@gmail.com>
2018-08-15 12:27:54 -0500

    Configuracion de comandos base y listar con la libreria npm yargs

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,11 +1,27 @@
+const argv = require('yargs')
+             .command('listar','Imprime en consola la tabla de multiplicar',{
+                 base:{
+                     demand:true,
+                     alias:'b'
+                 },
+                 limite:{
+                     alias:'l',
+                     default:10
+                 }
+             })
+             .help()
+             .argv;
 const {crearArchivo} = require('./multiplicar/multiplicar');
 
 //leer parametros.
-let argv = process.argv;
-let parametro = argv[2];
-let base = parametro.split('=')[1];
+let argv2 = process.argv;
+// let parametro = argv[2];
+// let base = parametro.split('=')[1];
 
-console.log(base);
+console.log("Base: ",argv.base);
+console.log("Limite: ",argv.base);
+
+return;
 
 crearArchivo(base)
     .then(archivo=>{
