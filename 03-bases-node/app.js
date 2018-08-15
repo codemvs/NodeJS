17c72ffe9bf9d1c55df3a4f5715f5507eefbaac2
codemvs <codemvs@gmail.com>
2018-08-15 11:00:57 -0500

    03-base-node: write txt

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,5 +1,15 @@
-let base = 2;
+/**
+ * Requireds
+ */
+const fs = require('fs');
 
+let base = 35;
+let multiplicacion='';
 for (let i = 1; i <= 10; i++) {
-    console.log(`${base} * ${i} = ${base*i}`);        
+    multiplicacion+=`${base} * ${i} = ${base*i}\n`;
 }
+
+fs.writeFile(`./tablas/tabla-${base}.txt`,multiplicacion,err=>{
+    if(err)throw err;
+    console.log(`Archivo tabla-${base}.txt ha sido creado`);     
+});
