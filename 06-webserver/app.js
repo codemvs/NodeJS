5da8d370f86459fd9f614085f9eda767b0a5127a
codemvs <codemvs@gmail.com>
2018-09-04 14:37:08 -0500

    06: ejemplo http Node

---
+++ 06-webserver/app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+http.createServer((req,res)=>{
+    res.writeHead(200,{'Content-Type':'application/json'});
+    res.write(JSON.stringify({
+        nombre:"Salvador",
+        edad:25,
+        url:req.url
+    }));
+    res.end();
+})
+.listen(8080);
+
+console.log('Escuchando el puerto 8080');
